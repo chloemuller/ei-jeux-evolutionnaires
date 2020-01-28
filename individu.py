9a9e512e35ccd514e435317aa00589b6c85a0f62
Gauthier Roy <gauthierroy83@gmail.com>
2020-01-28 09:10:05 +0100

    fixes in Individu

--- individu.py
+++ individu.py
@@ -1,33 +1,40 @@
-from table_rotation import table_rotation
-from traj3D import *
+from RotTable import RotTable
+from Traj3D import *
 import numpy as np
 from math import sqrt
 
 class Individu():
 
-    def __init__(self, rot_table):
-        self.rot_table = rot_table
+    def __init__(self, table):
+        self.table = table
         self.score = None
     
     def evaluate(self, brin):
         traj = Traj3D()
-        traj.compute(brin, self.rot_table)
+        traj.compute(brin, self.table)
         traj_array = np.array(traj.getTraj())
 
-        first_nucleotide = traj_array[0, :]
-        last_nucleotide = traj_array[-1, :]
-        distance = sqrt(sum((last_nucleotide - last_nucleotide) ** 2))
+        first_nucleotide = traj_array[0, 0:3]
+        last_nucleotide = traj_array[-1, 0:3]
+        distance = sqrt(sum((first_nucleotide - last_nucleotide) ** 2))
 
         first_name = brin[0]
         last_name = brin[-1]
 
-        rot_computed = rot_table[last_name+first_name]
-        rot_traj = first_name - last_name
+        rot_computed = self.table.Rot_Table[last_name+first_name]
+        rot_traj = first_nucleotide - last_nucleotide
+        print(rot_traj)
+        print(rot_computed)
         diff_angle = sum(abs(rot_computed - rot_traj))
 
         self.score = 1/(distance + diff_angle)
         
     
     def mutation(self):
-
+        mutation = 0
         return mutation
+
+table = RotTable()
+test = Individu(table)
+test.evaluate("AAAGGATCTTCTTGAGATCCTTTTTTTCTGCGCGTAATCTGCTGCCAGTAAACGAAAAAACCGCCTGGGGAGGCGGTTTAGTCGAA")
+print(test.score)
