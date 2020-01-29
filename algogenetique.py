1639a59be565b8ac15449d424de66fa5f3d693e7
Loic <loic.busson@student-cs.fr>
2020-01-29 09:54:50 +0100

    pull

--- algogenetique.py
+++ algogenetique.py
@@ -23,21 +23,15 @@ def main(N,tmax,pmutation, proportion,brin="plasmid_8k.fasta"):
             if individu.score>max:
                 best=individu
                 max=individu.score
-        L.append(max)
-        print(i,":",max)
+        L.append(1/max)
+        print(i,":",1/max)
 
     plt.plot([i for i in range(tmax)], L)
     plt.show()
     return(best, People)
 
 
-best, People = main(60,100,0.01,20)
-traj = Traj3D()
-traj.compute(best.brin,best.table)
-traj.draw("plot")
-
-
-best,People = main(100,100,0.01,50)
+best,People = main(60,100,0.01,25)
 traj = Traj3D()
 traj.compute(best.brin,best.table)
 traj.draw("plot")
