d06c693ffcdc42dbc94f23c4a167e89784e78caf
Kappes Marques Rodrigo <rodrigo.kappes@student-cs.fr>
2020-01-30 14:03:36 +0100

    Revert "FUCK THIS"
    This reverts commit 562809b65a8d44ab5b9196c321a2e29c28695383

--- population.py
+++ population.py
@@ -133,7 +133,7 @@ class Population:
         if proba_mutation == None :
             proba_mutation = 0.001
         if selection == None :
-            selection = self.selection_duel
+            selection = self.selection_par_rang
         else :
             selection = liste_selections[selection]
         if p == None :
